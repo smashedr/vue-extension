6c6731cefaa6665c9e9584ba8a4d9aa0e1bd3315
Shane <6071159+smashedr@users.noreply.github.com>
2026-03-02 19:53:34 -0800

    Add Shortcuts Component

---
+++ src/components/KeyboardShortcuts.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+const isFirefox = typeof browser !== 'undefined' && typeof browser?.runtime?.getBrowserInfo === 'function'
+console.debug('isFirefox:', isFirefox)
+
+function openChromeShortcuts() {
+  chrome.tabs.update({ url: 'chrome://extensions/shortcuts' })
+}
+</script>
+
+<template>
+  <table id="keyboard-shortcuts" class="table table-sm table-borderless table-hover d-none">
+    <caption class="visually-hidden">
+      Keyboard Shortcuts
+    </caption>
+    <thead class="visually-hidden">
+      <tr>
+        <th>Description</th>
+        <th>Shortcut</th>
+      </tr>
+    </thead>
+    <tbody></tbody>
+    <tfoot class="d-none">
+      <tr>
+        <td class="bg-transparent">
+          <i class="fa-regular fa-keyboard me-1"></i>
+          <span class="description"></span>
+        </td>
+        <td class="bg-transparent text-end" title="Keyboard Shortcut">
+          <kbd>Unknown</kbd>
+        </td>
+      </tr>
+    </tfoot>
+  </table>
+  <!-- keyboard-shortcuts -->
+
+  <div class="mb-2">
+    Manage Keyboard Shortcuts:
+    <a
+      v-if="isFirefox"
+      class="text-decoration-none d-inline-block firefox"
+      href="https://support.mozilla.org/en-US/kb/manage-extension-shortcuts-firefox"
+      target="_blank"
+      rel="noopener"
+    >
+      https://mzl.la/3Qwp5QQ
+      <i class="fa-solid fa-arrow-up-right-from-square fa-xs"></i
+    ></a>
+    <a v-if="!isFirefox" id="chrome-shortcuts" class="d-inline-block chrome" role="button" @click="openChromeShortcuts"
+      >chrome://extensions/shortcuts</a
+    >
+  </div>
+</template>
+
+<!--<style scoped></style>-->
--- src/options/App.vue
+++ src/options/App.vue
@@ -6,6 +6,7 @@ import BackToTop from '@/components/BackToTop.vue'
 import PermissionCheck from '@/components/PermissionCheck.vue'
 import ToastAlerts from '@/components/ToastAlerts.vue'
 import OptionsControls from '@/components/OptionsControls.vue'
+import KeyboardShortcuts from '@/components/KeyboardShortcuts.vue'
 
 console.debug('%c options/App.vue', 'color: Lime')
 
@@ -15,13 +16,6 @@ onMounted(() => {
 
 const manifest = chrome.runtime.getManifest()
 console.debug('manifest:', manifest)
-
-const isFirefox = typeof browser !== 'undefined' && typeof browser?.runtime?.getBrowserInfo === 'function'
-console.debug('isFirefox:', isFirefox)
-
-function openChromeShortcuts() {
-  chrome.tabs.update({ url: 'chrome://extensions/shortcuts' })
-}
 </script>
 
 <template>
@@ -73,52 +67,7 @@ function openChromeShortcuts() {
             <hr class="w-100 my-0" />
           </div>
 
-          <table id="keyboard-shortcuts" class="table table-sm table-borderless table-hover d-none">
-            <caption class="visually-hidden">
-              Keyboard Shortcuts
-            </caption>
-            <thead class="visually-hidden">
-              <tr>
-                <th>Description</th>
-                <th>Shortcut</th>
-              </tr>
-            </thead>
-            <tbody></tbody>
-            <tfoot class="d-none">
-              <tr>
-                <td class="bg-transparent">
-                  <i class="fa-regular fa-keyboard me-1"></i>
-                  <span class="description"></span>
-                </td>
-                <td class="bg-transparent text-end" title="Keyboard Shortcut">
-                  <kbd>Unknown</kbd>
-                </td>
-              </tr>
-            </tfoot>
-          </table>
-          <!-- keyboard-shortcuts -->
-
-          <div class="mb-2">
-            Manage Keyboard Shortcuts:
-            <a
-              v-if="isFirefox"
-              class="text-decoration-none d-inline-block firefox"
-              href="https://support.mozilla.org/en-US/kb/manage-extension-shortcuts-firefox"
-              target="_blank"
-              rel="noopener"
-            >
-              https://mzl.la/3Qwp5QQ
-              <i class="fa-solid fa-arrow-up-right-from-square fa-xs"></i
-            ></a>
-            <a
-              v-if="!isFirefox"
-              id="chrome-shortcuts"
-              class="d-inline-block chrome"
-              role="button"
-              @click="openChromeShortcuts"
-              >chrome://extensions/shortcuts</a
-            >
-          </div>
+          <KeyboardShortcuts />
         </div>
         <!-- data-mobile-add -->
 
@@ -163,13 +112,10 @@ function openChromeShortcuts() {
             >Open Issue</a
           >
         </div>
-        <!-- footer -->
+        <!-- #options-wrapper -->
       </div>
-      <!-- options-wrapper -->
     </div>
-    <!-- m-auto -->
   </div>
-  <!-- d-flex -->
 
   <ToastAlerts />
 
